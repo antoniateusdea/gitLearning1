7419149f32e84a76b040df9565044414fad2cd16
antoniateusdea <antonia.teusdea@icloud.com>
2020-05-07 18:54:59 +0300

    add Spring.java

---
+++ src/tests/Spring.java
@@ -0,0 +1,11 @@
+package tests;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Spring {
+    public static void main(String[] args) {
+        System.out.println("Goodbye Spring!...");
+    }
+
+
+}
